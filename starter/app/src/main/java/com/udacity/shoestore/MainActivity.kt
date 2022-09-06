acfb97c1e89741b6f0d3bc8815161380209b59d7
TheBoss000 <Bossmail28@gmail.com>
2022-09-06 15:44:00 +0200

    made gradle crash and not able to run the app

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.udacity.shoestore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.udacity.shoestore.viewmodels.AppViewModel
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    lateinit var viewModel: AppViewModel
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_main)
-        Timber.plant(Timber.DebugTree())
+//        Timber.plant(Timber.DebugTree())
+        viewModel = ViewModelProvider(this)[AppViewModel::class.java]
+
     }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//
+//    }
 }
---
+++ starter/app/src/main/java/com/udacity/shoestore/uis/RegisterFragment.kt
@@ -0,0 +1,4 @@
+package com.udacity.shoestore.uis
+
+class RegisterFragment {
+}
