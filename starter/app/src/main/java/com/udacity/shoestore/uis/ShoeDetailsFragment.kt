3106ed8e28991e1480a3f2b82b10b141b55401e7
TheBoss000 <Bossmail28@gmail.com>
2022-09-08 01:16:14 +0200

    adjusted layouts navigation and views

--- starter/app/src/main/java/com/udacity/shoestore/uis/ShoeDetailsFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/uis/ShoeDetailsFragment.kt
@@ -1,6 +1,37 @@
 package com.udacity.shoestore.uis
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.udacity.shoestore.databinding.ShowDetailsFragmentBinding
+import com.udacity.shoestore.viewmodels.AppViewModel
 
-class ShoeDetailsFragment: Fragment() {
+class ShoeDetailsFragment : Fragment() {
+    lateinit var binding: ShowDetailsFragmentBinding
+    lateinit var viewModel: AppViewModel
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = ShowDetailsFragmentBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(requireActivity())[AppViewModel::class.java]
+
+        viewModel.shoeList.observe(viewLifecycleOwner) {
+            clearLinearLayout()
+            renderListToLayout()
+        }
+
+        return binding.root
+    }
+
+    private fun renderListToLayout() {
+        TODO("Not yet implemented")
+    }
+
+    private fun clearLinearLayout() {
+    }
 }
--- starter/app/src/main/java/com/udacity/shoestore/viewmodels/AppViewModel.kt
+++ starter/app/src/main/java/com/udacity/shoestore/viewmodels/AppViewModel.kt
@@ -1,6 +1,14 @@
 package com.udacity.shoestore.viewmodels
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.udacity.shoestore.models.Shoe
 
-class AppViewModel: ViewModel() {
+class AppViewModel: ViewModel(){
+
+    val shoeList = MutableLiveData<MutableList<Shoe>>()
+
+    fun addShoe(shoe: Shoe) {
+      shoeList.value?.add(shoe)
+    }
 }
