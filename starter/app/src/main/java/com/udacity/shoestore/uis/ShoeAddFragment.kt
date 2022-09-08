6b3f498c932b28e2060d2d2ccc979b3d42bd7f68
TheBoss000 <Bossmail28@gmail.com>
2022-09-08 03:53:14 +0200

    finishing project to submit

--- starter/app/src/main/java/com/udacity/shoestore/uis/ShoeAddFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/uis/ShoeAddFragment.kt
@@ -5,18 +5,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.ShoeAddFragmentBinding
+import com.udacity.shoestore.models.Shoe
+import com.udacity.shoestore.viewmodels.AppViewModel
 
 class ShoeAddFragment : Fragment() {
     lateinit var binding: ShoeAddFragmentBinding
+    lateinit var viewModel: AppViewModel
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = ShoeAddFragmentBinding.inflate(inflater, container, false)
-
-
+        viewModel = ViewModelProvider(requireActivity())[AppViewModel::class.java]
+        binding.button4.setOnClickListener {
+            findNavController().navigate(R.id.goto_shoeDetailsFragment_from_shoeAddFragment)
+            addShoe()
+        }
         return binding.root
+    }
+
+    private fun addShoe() {
+        val name = binding.editTextTextPersonName3.text.toString()
+        val size = binding.editTextTextPersonName4.text.toString()
+        val company = binding.editTextTextPersonName5.text.toString()
+        val desc = binding.editTextTextPersonName6.text.toString()
+        val shoe = Shoe(name = name, size = size.toDouble(), company = company, description = desc)
+        viewModel.addShoe(shoe)
     }
 }
