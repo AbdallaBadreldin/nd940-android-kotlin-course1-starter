72abd954ea76888676694322ba21830a414d4f8f
TheBoss000 <Bossmail28@gmail.com>
2022-09-09 00:27:20 +0200

    added two way data binding and xml comment

--- starter/app/src/main/java/com/udacity/shoestore/uis/ShoeAddFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/uis/ShoeAddFragment.kt
@@ -9,7 +9,6 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.ShoeAddFragmentBinding
-import com.udacity.shoestore.models.Shoe
 import com.udacity.shoestore.viewmodels.AppViewModel
 
 class ShoeAddFragment : Fragment() {
@@ -20,21 +19,25 @@ class ShoeAddFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         binding = ShoeAddFragmentBinding.inflate(inflater, container, false)
         viewModel = ViewModelProvider(requireActivity())[AppViewModel::class.java]
         binding.button4.setOnClickListener {
             findNavController().navigate(R.id.goto_shoeDetailsFragment_from_shoeAddFragment)
             addShoe()
         }
+        binding.appViewModel = viewModel
+        binding.lifecycleOwner = this
+
         return binding.root
     }
 
     private fun addShoe() {
-        val name = binding.editTextTextPersonName3.text.toString()
-        val size = binding.editTextTextPersonName4.text.toString()
-        val company = binding.editTextTextPersonName5.text.toString()
-        val desc = binding.editTextTextPersonName6.text.toString()
-        val shoe = Shoe(name = name, size = size.toDouble(), company = company, description = desc)
-        viewModel.addShoe(shoe)
+//        val name = binding.editTextTextPersonName3.text.toString()
+//        val size = binding.editTextTextPersonName4.text.toString()
+//        val company = binding.editTextTextPersonName5.text.toString()
+//        val desc = binding.editTextTextPersonName6.text.toString()
+//        val shoe = Shoe(name = name, size = size.toDouble(), company = company, description = desc)
+        viewModel.addShoe()
     }
 }
